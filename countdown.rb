b1a6f14d1359b5931cb757410680b9de00de84a1
Steven Laff <steve.laff@gmail.com>
2019-08-17 03:16:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,13 +2,17 @@
 
 def countdown (t_minus)
   until t_minus == 0 do
-    if t_minus <= 10
-      puts "#{t_minus} SECOND(S)!"
-    end
+    puts "#{t_minus} SECOND(S)!"
     t_minus -= 1
-    sleep 1
   end
   puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep
+  until t_minus == 0 do
+    puts "#{t_minus} SECOND(S)!"
+    t_minus -= 1
+    sleep 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
