754c9f8be236d59674222321b14cd97caa322ec9
Steven Laff <steve.laff@gmail.com>
2019-08-17 03:09:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown (t_minus)
+  until t_minus == 0 do
+    if t_minus > 1
+      puts "#{t_minus} SECONDS!"
+    else
+      puts "#{t_minus} SECOND!"
+    end
+  end
 end
